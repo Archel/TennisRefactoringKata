77e1ea7bae45ae691e664e3bccdfe4f78733cb7a
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-20 14:56:49 +0100

    do some refactor to encapsulate some concepts of our domain

--- TennisGame1.js
+++ TennisGame1.js
@@ -2,78 +2,96 @@ const LOVE = 0;
 const FIFTEEN = 1;
 const THIRTY = 2;
 const FORTY = 3;
-const RegularScores = {};
-RegularScores[LOVE] = "Love";
-RegularScores[FIFTEEN] = "Fifteen";
-RegularScores[THIRTY] = "Thirty";
-RegularScores[FORTY] = "Forty";
+const Score = {};
+Score[LOVE] = "Love";
+Score[FIFTEEN] = "Fifteen";
+Score[THIRTY] = "Thirty";
+Score[FORTY] = "Forty";
 
 const SCORE_SEPARATOR = "-";
+let _playerOneName, _playerTwoName;
 
 var TennisGame1 = function(playerOneName, playerTwoName) {
-    this.playerOneScore = 0;
-    this.playerTwoScore = 0;
-    this.playerOneName = playerOneName;
-    this.playerTwoName = playerTwoName;
+    this.scores = {};
+    this.scores[playerOneName] = 0;
+    this.scores[playerTwoName] = 0;
+    _playerOneName = playerOneName;
+    _playerTwoName = playerTwoName;
+};
+
+TennisGame1.prototype.playerOneScore = function() {
+    return this.scores[_playerOneName];
+};
+
+TennisGame1.prototype.playerTwoScore = function() {
+    return this.scores[_playerTwoName];
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (this.playerOneName === playerName)
-        this.playerOneScore += 1;
-    else
-        this.playerTwoScore += 1;
+    this.scores[playerName] += 1;
 };
 
-TennisGame1.prototype.getEqualsScore = function(playerOneScore) {
-    if (this.playerOneScore > 2) { return 'Deuce'; }
-    return RegularScores[this.playerOneScore] + '-All';
+TennisGame1.prototype.getEqualsScore = function() {
+    let score = this.playerOneScore();
+    if (score > 2) { return 'Deuce'; }
+    return Score[score] + '-All';
 }
 
 TennisGame1.prototype.scoreIsEqual = function() {
-    return this.playerOneScore === this.playerTwoScore;
+    return this.playerOneScore() === this.playerTwoScore();
 }
 
 TennisGame1.prototype.scoreIsAdvantage = function() {
-    return this.playerOneScore >= 4 || this.playerTwoScore >= 4;
+    return this.playerOneScore() >= 4 || this.playerTwoScore() >= 4;
 }
 
 TennisGame1.prototype.determinePlayersAdvantage = function() {
-    const advantagePlayer = this.playerOneScore > this.playerTwoScore ? this.playerOneName : this.playerTwoName
-    return 'Advantage ' + advantagePlayer;
+    const advantagePlayer = this.playerOneScore() > this.playerTwoScore() ? _playerOneName : _playerTwoName;
+    return 'Advantage ' + this.highestScoringPlayer();
 }
 
-TennisGame1.prototype.isAdvantatge = function() {
-    return Math.abs(this.playerOneScore - this.playerTwoScore) === 1;
+TennisGame1.prototype.highestScoringPlayer = function() {
+    if (this.scoreIsEqual()) return null;
+    return this.playerOneScore() > this.playerTwoScore() ? _playerOneName : _playerTwoName;
+}
+
+TennisGame1.prototype.isAdvantage = function() {
+    return this.differenceBetweenPlayerScores() === 1;
 }
 
 TennisGame1.prototype.isWon = function() {
-    return Math.abs(this.playerOneScore - this.playerTwoScore) >= 2;
+    return this.differenceBetweenPlayerScores() >= 2;
 }
 
 TennisGame1.prototype.determineAdvantageScore = function() {
-    const scoreDifference = this.playerOneScore - this.playerTwoScore;
+    const scoreDifference = this.differenceBetweenPlayerScores();
     if (this.isWon()) {
-        return "Win for " + (scoreDifference >= 2 ? this.playerOneName : this.playerTwoName);
+        return "Win for " + this.highestScoringPlayer();
     }
     
     return this.determinePlayersAdvantage();
 }
 
 TennisGame1.prototype.determineRegularScore = function() {
-    return RegularScores[this.playerOneScore] + SCORE_SEPARATOR + RegularScores[this.playerTwoScore];
+    return Score[this.playerOneScore()] + SCORE_SEPARATOR + Score[this.playerTwoScore()];
 }
 
 TennisGame1.prototype.getScore = function() {
-    let score = "";
+    
     if (this.scoreIsEqual()) {
-        score = this.getEqualsScore(this.playerOneScore);
-    } else if (this.scoreIsAdvantage()) {
-        score = this.determineAdvantageScore();
-    } else {
-        score = this.determineRegularScore();
+        return this.getEqualsScore();
+    } 
+    
+    if (this.scoreIsAdvantage()) {
+        return this.determineAdvantageScore();
     }
-    return score;
+
+    return this.determineRegularScore();
 };
+
+TennisGame1.prototype.differenceBetweenPlayerScores = function() {
+    return Math.abs(this.scores[_playerOneName] - this.scores[_playerTwoName]);
+}
 
 if (typeof window === "undefined") {
     module.exports = TennisGame1;
